607aca38c410b9058b314803b7e6f7b5ca6ca0b1
ahmet umut <umut.akkas@metu.edu.tr>
2022-11-01 01:44:45 +0300

    torus world and ball hits
    Now the world is a torus and the balls can hit the agents. Increasing the damage of the hit agent and the score of the emittor.

--- basic game.py
+++ basic game.py
@@ -3,7 +3,7 @@ from random import randrange as ranran
 import arcade as arcad
 from math import sin,cos,pi
 
-colos= [255,255,255*1], [255,0*1,255], [0]*3, [0]*3
+colos= [255,255,255*1], [255,0*1,255], [0]*3, [0]*3, [0]*3
 for a in range(len(colos)): exec(f"colo{a}=colos[a]")
 
 width=999
@@ -30,24 +30,33 @@ class objec:
 class Ball(objec):
 	"""docstring for Ball"""
 	def __init__(ball,agent, radiu=4, colo=colo3, veloc=0):
+		#maybe a lifetime thing can be added
 		super().__init__(agent.x,agent.y, radiu,colo)
 		ball.vx = cos(agent.dire)*veloc
 		ball.vy = sin(agent.dire)*veloc
-	def updat(ball):
+		ball.agent = agent
+		ball.tbdeletd =False#to be deleted
+	def updat(ball,dtime):
 		super().updat(dtime)
 		ball.x %= width
-		ball.y %= heighb
+		ball.y %= heigh
+		if ball.tbdeletd:
+			ball.agent.game.balls.remove(ball)
+			del(ball)
+	def delet(ball): ball.tbdeletd =True#delete
 
 
 class Agent(objec):
 	"""docstring for Agent"""
-	def __init__(agent,game, mass=1, **paras):
+	def __init__(agent,game, mass=9, **paras):
 		super().__init__(**paras)
+		agent.damag =0
+		agent.score =0
 		agent.game =game
 		agent.mass = mass
 		agent.dire,agent.vdire,agent.adire = [0]*3#dire: direction in radiants.
 		agent.fthru = 0#thrust force
-		agent.thrum = 9#thrusting multiplier: reflects the thrusting power of the agent
+		agent.thrum = 99#thrusting multiplier: reflects the thrusting power of the agent
 	def draw(agent):
 		super().draw()
 		dralin(*agent.posit, agent.x + cos(agent.dire)*agent.radiu*2, agent.y + sin(agent.dire)*agent.radiu*2, colo2)#draws the directipn line
@@ -58,8 +67,11 @@ class Agent(objec):
 		agent.dire += dtime * (agent.vdire + dtime*agent.adire/2)
 		agent.dire %= pi*2
 		agent.vdire += dtime * agent.adire
+		agent.x %= width
+		agent.y %= heigh
 	def fire(agent):
 		agent.game.balls.append(Ball(agent, veloc=99))
+
 
 
 class Game(windo):
@@ -69,22 +81,35 @@ class Game(windo):
 		game.agents = []
 		game.balls = []
 	def setup(game):
+		# sched(game.cofps,4)
 		sebaco(colo0)
 
 		game.aagent(x=111,y=111)
+		game.aagent(x=222,y=222, colo=colo4)
 		for a in range(len(game.agents)): exec(f"game.agent{a}=game.agents[a]")
 
 		game.agent0.vx = 6
 		
 	def aagent(game,**paras):#add agent
 		game.agents.append(Agent(game,**paras))
+	def cofps(game,dtime):
+		print(f"FPS: {round(game.fpsco/dtime)}")
+		game.fpsco =0
 	def on_draw(game):
 		staren()
 		for b in game.balls: b.draw()
 		for a in game.agents: a.draw()
 	def on_update(game,dtime):#delta time
-		for a in game.agents: a.updat(dtime)
+		game.fpsco +=1
+		for a in game.agents:
+			a.updat(dtime)
+			for b in game.balls:#collision check
+				if b.agent!=a and abs(b.x-a.x) < b.radiu+a.radiu and abs(b.y-a.y) < a.radiu+b.radiu and ((b.x-a.x)**2+(b.y-a.y)**2)**.5<a.radiu+b.radiu:
+					a.damag +=1#it may depend on b in next versions
+					b.agent.score +=1
+					b.delet()#b may not be deleted
 		for b in game.balls: b.updat(dtime)
+
 	def on_key_press(game,ke,modif):
 		agent= game.agent0
 		match ke:
